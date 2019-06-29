63a249a7495c68084bf3069f9e997335670852e2
shobhit gupta <shobhit90gupta@gmail.com>
2019-06-29 15:08:05 +0530

    updated pet.js to show more beautiful UI with images and formatting. added reach/router (better than react router) added details page to demonstrate reach router.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./searchParams";
+import Details from "./details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams></SearchParams>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/"></SearchParams>
+          <Details path="/details/:id"></Details>
+        </Router>
       </div>
     </React.StrictMode>
   );
---
+++ src/details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Details = () => {
+
+    return (
+        <div>
+            <h1>Hi lol</h1>
+        </div>
+    );
+}
+
+export default Details;
